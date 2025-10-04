d34188908166dddb47230b4ca79245d86c8971e1
Dilshad Ahmad <dilshadsoraon09@gmail.com>
2025-10-05 01:20:42 +0530

    changes in the code

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", {
-    title: "Express Tutorial",
+    title: "Express and Jenkins Tutorial",
     message: "This is the revision of Nodejs from scratch.",
   });
 });
